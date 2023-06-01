2e24c5939816019284fe96e981477dbf1750e961
“AbdulRehmanjr” <abdulrehman2020white@gmail.com>
2023-06-01 05:20:19 +0500

    changes

--- src/main/java/com/lawstack/app/controller/WebHook.java
+++ src/main/java/com/lawstack/app/controller/WebHook.java
@@ -1,20 +1,27 @@
 package com.lawstack.app.controller;
 
+
+import java.time.LocalDate;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lawstack.app.model.CardSubscription;
 import com.lawstack.app.model.Dashboard;
-import com.lawstack.app.model.Order;
 import com.lawstack.app.model.OrderPayment;
-import com.lawstack.app.model.PaymentRequest;
-import com.lawstack.app.model.Seller;
 import com.lawstack.app.model.User;
 import com.lawstack.app.model.UserDashboard;
 import com.lawstack.app.service.DashboardService;
 import com.lawstack.app.service.EmailService;
 import com.lawstack.app.service.OrderPaymentService;
-import com.lawstack.app.service.PaymentService;
 import com.lawstack.app.service.SellerService;
 import com.lawstack.app.service.SubscriptionService;
 import com.lawstack.app.service.UserDashBoardService;
@@ -29,38 +36,18 @@ import com.stripe.model.Price;
 import com.stripe.model.Product;
 import com.stripe.model.StripeObject;
 import com.stripe.model.Subscription;
+
 import com.stripe.model.checkout.Session;
 import com.stripe.net.Webhook;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.time.LocalDate;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-
-
-@RestController
-@RequestMapping("/checkout")
+@RequestMapping("/webhook")
 @Slf4j
-public class CheckoutController {
+@RestController
+public class WebHook {
 
-    @Autowired
-    private PaymentService paymentService;
-
-
-
-    private String customerId = "";
-
-
-     
+    
     @Autowired
     private DashboardService dashService;
 
@@ -69,6 +56,8 @@ public class CheckoutController {
 
     @Autowired
     private UserService userService;
+
+    private String customerId = "";
 
     @Autowired
     private SubscriptionService subService;
@@ -264,62 +253,5 @@ public class CheckoutController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
-    
-    @PostMapping("/cancel/{email}")
-    ResponseEntity<String> cancelSubscription(@PathVariable String email) {
 
-        log.info("Request to cancel the subscription");
-        String id = this.paymentService.getSubscriptionId(email);
-
-        Subscription subscription;
-        try {
-            subscription = Subscription.retrieve(id);
-            this.customerId = subscription.getCustomer();
-            subscription.cancel();
-            Seller seller = this.sellerService.getByEmail(email);
-
-            seller.setSellerType("NONE");
-
-            this.sellerService.updateJobStatus(seller);
-        } catch (StripeException e) {
-            log.error("Error: {} Message: {}", e.getStatusCode(), e.getMessage());
-            return ResponseEntity.status(404).body(null);
-        }
-
-        return ResponseEntity.status(201).body("canceled");
-
-    }
-
-    @PostMapping("/create-checkout-session")
-    public ResponseEntity<?> createCheckoutSession(@RequestBody PaymentRequest payment) {
-
-        log.info("Request for checkout page received");
-
-        String url = this.paymentService.paymentCheckout(payment.getType(), payment.getEmail());
-
-        if (url != null) {
-
-            return ResponseEntity.status(201).body(url);
-        }
-        return ResponseEntity.status(404).body(null);
-
-    }
-
-    @PostMapping("/project")
-    public ResponseEntity<?> projectSession(@RequestBody Order order) {
-
-        log.info("Request for checkout page received");
-
-        String s = this.paymentService.projectPayment(order);
-
-        if (s != null) {
-
-            return ResponseEntity.status(201).body(s);
-        }
-
-        return ResponseEntity.status(404).body(null);
-
-    }
-
-   
 }
